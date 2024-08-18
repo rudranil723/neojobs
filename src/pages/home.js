4329f151f8220b00ca28068be1bdec7cd9969cc6
Ruddy <rudranilshil@gmail.com>
2024-08-18 20:55:48 +0530

    home page photo added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,11 @@
 import React from 'react'; 
+import '../sass/home.scss';
 
 const Home = () => {
   return (
     <div>
       {/* <Navbar /> */}
       <div className="home-content">
-        <h1>Welcome to My Website</h1>
-        <p>This is the home page of my website.</p>
       </div>
     </div>
   );
