2b4b7b33f187df022b21dfa4d7c4796879548087
Ruddy <rudranilshil@gmail.com>
2024-08-19 10:32:06 +0530

    happy with the search bar

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,9 +7,10 @@ const Home = () => {
       {/* <Navbar /> */}
       <div className="home-content">
         <h1>Find Jobs</h1>
-        <p>Hire Experts or be hired in sales & marketing|</p>
+        <p>Hire Experts or be hired in sales & marketing</p>
         <div className="search-bar">
           <input type="text" placeholder="Tell us what you are looking for?" />
+          <button type="button">Search</button>
         </div>
       </div>
     </div>
