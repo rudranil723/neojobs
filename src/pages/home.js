f0dedcfd9e0ef6e902e0aacbaf035287095574b6
Ruddy <rudranilshil@gmail.com>
2024-08-19 10:18:43 +0530

    font chnage and some update

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,11 @@ const Home = () => {
     <div>
       {/* <Navbar /> */}
       <div className="home-content">
+        <h1>Find Jobs</h1>
+        <p>Hire Experts or be hired in sales & marketing|</p>
+        <div className="search-bar">
+          <input type="text" placeholder="Tell us what you are looking for?" />
+        </div>
       </div>
     </div>
   );
