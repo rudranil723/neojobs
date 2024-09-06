433dcace9ec0aba6f3f15c406d711638f5abb441
Ruddy <rudranilshil@gmail.com>
2024-09-06 11:55:11 +0530

    okay 2

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -28,6 +28,48 @@ const JobsPage = () => {
         </div>
         <button className="apply-btn">Apply for Job</button>
       </div>
+
+      <div className="content-grid">
+        <div className="video-section">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/PFkzyLHiEPw"
+            title="YouTube video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <div className="video-description">
+              <p>The Heavy Equipment / Grader Operator is responsible for operating one or several types of power construction equipment, such as front end loader, roller, bulldozer, or excavator in order to move, excavate, and grade earth, erect structures, or pour concrete or other hard surface pavements.</p>
+          </div>
+        </div>
+
+        <div className="job-overview">
+          <h2>Job Overview</h2>
+          <ul>
+            <li>
+              <FaCalendarAlt className="icon" /> Date Posted: <span>Posted 5 years ago</span>
+            </li>
+            <li>
+              <FaCalendarAlt className="icon" /> Expiration Date: <span>April 15, 2027</span>
+            </li>
+            <li>
+              <FaMapMarkerAlt className="icon" /> Location: <span>Munchen, Germany</span>
+            </li>
+            <li>
+              <FaClock className="icon" /> Hours: <span>44h / week</span>
+            </li>
+            <li>
+              <FaDollarSign className="icon" /> Rate: <span>$15 - $25 / hour</span>
+            </li>
+            <li>
+              <FaDollarSign className="icon" /> Salary: <span>$35,000.00 - $38,000.00</span>
+            </li>
+          </ul>
+          <button className="apply-btn">Apply for job</button>
+        </div>
+      </div>
     </div>
   );
 };
