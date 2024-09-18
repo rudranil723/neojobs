11993dc64191ada8515b4586d8d775b7409e2bd6
Ruddy <rudranilshil@gmail.com>
2024-09-19 03:20:08 +0530

    category 10%

--- src/freelancer/bycategories.js
+++ src/freelancer/bycategories.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './bycategories.scss';
 
 const Categories = () => {
     return (
-        <div>
-            <h1>ferehtrgver</h1>
+        <div className='category-page'>
+            <div className="top-section">
+                <h1 className="page-title">Browse By Location</h1>
+                <div className="button-group">
+                    <button className="post-project">Post a Project</button>
+                    <button className="sign-up">Sign up for work</button>
+                </div>
+            </div>
+            <hr className="divider" />
         </div>
     );
 };
